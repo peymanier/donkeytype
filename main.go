32161b226041dfc47dc1ac3c56281831d3eb515b
Peyman <hassani.peyman627@gmail.com>
2025-07-13 11:20:15 +0330

    Setup bubble tea

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func main() {
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
+}
+
+type model struct {
+	text string
+}
+
+func initialModel() model {
+	return model{
+		text: "this is going to be a very long text",
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		}
+
+		return m, nil
+	}
+
+	return m, nil
+}
+
+func (m model) View() string {
+	return m.text
+}
