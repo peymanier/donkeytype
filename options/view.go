f5c9708c015c8d0f0b6943b9c147ddf4cccb2925
Peyman <hassani.peyman627@gmail.com>
2025-08-08 15:11:05 +0330

    Update the list of choices to showup beside options

--- options/view.go
+++ options/view.go
@@ -1,11 +1,19 @@
 package options
 
+import "github.com/charmbracelet/lipgloss"
+
 func (m Model) View() string {
 	if m.selectedOption != nil {
 		if m.selectedOption.input.Focused() {
 			return m.selectedOption.input.View()
 		}
-		return m.selectedOption.list.View()
+
+		m.list.SetShowHelp(false)
+		return lipgloss.JoinHorizontal(
+			lipgloss.Top,
+			m.list.View(),
+			m.selectedOption.list.View(),
+		)
 	}
 
 	return m.list.View()
